3d9f70310dad64cc86cabc380f40439979a05931
aini021 <aininrfdlh0@gmail.com>
2026-02-21 20:23:20 +0700

    Update CartContext

--- context/CartContext.tsx
+++ context/CartContext.tsx
@@ -1,17 +1,19 @@
 'use client';
 import { createContext, useContext, useState, ReactNode } from 'react';
 
-// Tipe data untuk item di keranjang
 interface CartItem {
   id: number;
+  uniqueId: number; // Untuk membedakan barang yang sama yang dibeli berkali-kali
   title: string;
   price: number;
 }
 
 interface CartContextType {
   cart: CartItem[];
-  addToCart: (item: CartItem) => void;
+  addToCart: (item: Omit<CartItem, 'uniqueId'>) => void;
+  removeFromCart: (uniqueId: number) => void;
   cartCount: number;
+  totalPrice: number;
 }
 
 const CartContext = createContext<CartContextType | undefined>(undefined);
@@ -19,12 +21,18 @@ const CartContext = createContext<CartContextType | undefined>(undefined);
 export function CartProvider({ children }: { children: ReactNode }) {
   const [cart, setCart] = useState<CartItem[]>([]);
 
-  const addToCart = (item: CartItem) => {
-    setCart((prev) => [...prev, item]);
+  const addToCart = (item: Omit<CartItem, 'uniqueId'>) => {
+    setCart((prev) => [...prev, { ...item, uniqueId: Date.now() + Math.random() }]);
   };
 
+  const removeFromCart = (uniqueId: number) => {
+    setCart((prev) => prev.filter((item) => item.uniqueId !== uniqueId));
+  };
+
+  const totalPrice = cart.reduce((total, item) => total + item.price, 0);
+
   return (
-    <CartContext.Provider value={{ cart, addToCart, cartCount: cart.length }}>
+    <CartContext.Provider value={{ cart, addToCart, removeFromCart, cartCount: cart.length, totalPrice }}>
       {children}
     </CartContext.Provider>
   );
